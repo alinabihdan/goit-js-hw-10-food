39aeb044dfc065054e7a33a01a27d84c779de1e7
alinabihdan <alina.bigdan1@gmail.com>
2021-06-08 22:54:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import itemsTemplate from './templates/menu.hbs';
 import menu from './menu.json';
 
 const menuRef = document.querySelector('.js-menu');
-
 const markup = itemsTemplate(menu);
 
 menuRef.insertAdjacentHTML('beforeend', markup);
